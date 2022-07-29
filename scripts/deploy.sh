b6cb996b785ca22b0ba7932e9700618a0cd9ebf0
이윤구 <lee36656@duzon.com>
2022-07-29 11:01:59 +0900

    deploy - classpath 설정 변경

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -38,7 +38,12 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
+#nohup java -jar \
+#    -Dspring.config.location=classpath:/application.yml,classpath:/application-real.yml,classpath:/application-oauth.properties \
+#    -Dspring.profiles.active=real \
+#    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+
 nohup java -jar \
-    -Dspring.config.location=classpath:/application.yml,classpath:/application-real.yml,classpath:/application-oauth.properties \
+    -Dspring.config.location=classpath:/application.yml,classpath:/application-real.yml,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.yml \
     -Dspring.profiles.active=real \
     $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
