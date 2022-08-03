a0af13803bc098b96bc3e311424c432f644efbdc
이윤구 <lee36656@duzon.com>
2022-08-04 02:15:00 +0900

    index - backgroupd 이미지 추가 및 idle 포트 종료 sh 수정

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -13,7 +13,7 @@ if [ -z ${IDLE_PID} ]
 then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "> kill -15 $IDLE_PID"
+  echo "> sudo kill -15 $IDLE_PID"
   kill -15 ${IDLE_PID}
   sleep 5
 fi
